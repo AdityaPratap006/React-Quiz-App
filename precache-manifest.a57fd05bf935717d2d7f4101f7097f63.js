399da2c261e4027b22e6d4bcd9f4910f18292251
Aditya Pratap <adityapratap7344@gmail.com>
2019-08-17 21:58:34 +0530

    Updates

---
+++ precache-manifest.a57fd05bf935717d2d7f4101f7097f63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325a89e12882400808d4a155f5baed83",
+    "url": "/React-Quiz-App/index.html"
+  },
+  {
+    "revision": "3d4fc7c4f5d798a88d5f",
+    "url": "/React-Quiz-App/static/css/main.1db50137.chunk.css"
+  },
+  {
+    "revision": "684092b01cf59c1960fc",
+    "url": "/React-Quiz-App/static/js/2.3fa2e94c.chunk.js"
+  },
+  {
+    "revision": "3d4fc7c4f5d798a88d5f",
+    "url": "/React-Quiz-App/static/js/main.aedcf46c.chunk.js"
+  },
+  {
+    "revision": "f1d28c3638f1546d973e",
+    "url": "/React-Quiz-App/static/js/runtime~main.b1b1e0e5.js"
+  }
+]);
